9def2bb023f7710ecda611d51f142bda24d7f4b7
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-02-02 22:06:18 +0100

    feat: add useRefreshProps hook

---
+++ src/common/hooks/useRefreshProps.ts
@@ -0,0 +1,59 @@
+import {useRouter} from 'next/router';
+import queryString from 'query-string';
+import {useCallback, useEffect, useState} from 'react';
+
+import type {NextRouter} from 'next/router';
+import type {UrlObject} from 'url';
+
+type Url = UrlObject | string;
+
+type InferTransitionOptions<GenericNextRouter extends NextRouter> =
+  GenericNextRouter extends {
+    readonly push: (
+      url: Url,
+      as?: Url,
+      options?: infer TransitionOptions,
+    ) => unknown;
+  }
+    ? TransitionOptions
+    : never;
+
+export const useRefreshProps = () => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const routeChangeStartHandler = (
+      _nextUrl: string,
+      transitionOptions: InferTransitionOptions<typeof router>,
+    ) => {
+      if (transitionOptions.shallow) {
+        setIsRefreshing(true);
+      }
+    };
+    const routeChangeFinishHandler = () => {
+      setIsRefreshing((currentIsRefreshing) => currentIsRefreshing && false);
+    };
+
+    router.events.on('routeChangeStart', routeChangeStartHandler);
+    router.events.on('routeChangeError', routeChangeFinishHandler);
+    router.events.on('routeChangeComplete', routeChangeFinishHandler);
+
+    return () => {
+      router.events.off('routeChangeStart', routeChangeStartHandler);
+      router.events.off('routeChangeError', routeChangeFinishHandler);
+      router.events.off('routeChangeComplete', routeChangeFinishHandler);
+    };
+  }, [router.events]);
+
+  const refreshProps = useCallback(
+    (searchParams: Record<string, string>) => {
+      const url = `${router.asPath}?${queryString.stringify(searchParams)}`;
+
+      void router.replace(url, undefined);
+    },
+    [router],
+  );
+
+  return {isRefreshing, refreshProps};
+};
