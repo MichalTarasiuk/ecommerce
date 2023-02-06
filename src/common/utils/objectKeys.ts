32e6bac6cefefba0556c6866538f795fb0f8b47e
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-02-06 07:46:24 +0100

    fix: type infer of objectKeys helper

--- src/common/utils/objectKeys.ts
+++ src/common/utils/objectKeys.ts
@@ -1,5 +1,13 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions -- narrow type down */
 
+type ObjectKeys<
+  AnyObject extends Record<PropertyKey, unknown>,
+  Keys extends Exclude<keyof AnyObject, symbol> = Exclude<
+    keyof AnyObject,
+    symbol
+  >,
+> = ReadonlyArray<Keys extends number ? `${Keys}` : Keys>;
+
 export const objectKeys = <AnyObject extends Record<PropertyKey, unknown>>(
   object: AnyObject,
-) => Object.keys(object) as ReadonlyArray<keyof AnyObject>;
+) => Object.keys(object) as unknown as ObjectKeys<AnyObject>;
