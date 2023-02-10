6ec999eae176973cd9e8f3e41b518bfe61987e17
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-02-10 14:04:41 +0100

    feat: add guard for forbidden pathnames only in prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,19 @@
 import {NextResponse} from 'next/server';
 
 import {routes} from './common/consts/consts';
+import {isProduction} from './common/utils/nodeEnvironment';
 
 import type {NextRequest} from 'next/server';
 
-const forbiddenPathnames = ['/mockServiceWorker.js'];
+const forbiddenPathnames = [/\.(js|mjs|cjs)/g];
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
 
-  if (forbiddenPathnames.includes(url.pathname)) {
+  if (
+    isProduction() &&
+    forbiddenPathnames.some((matcher) => url.pathname.match(matcher))
+  ) {
     url.pathname = routes.home;
 
     return NextResponse.redirect(url);
