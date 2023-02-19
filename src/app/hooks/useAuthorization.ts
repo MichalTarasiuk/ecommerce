bfc0d60675f17853de80d6749cbbabecb21a906f
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-02-19 15:46:55 +0100

    feat: clean up useAuthorization hook

--- src/app/hooks/useAuthorization.ts
+++ src/app/hooks/useAuthorization.ts
@@ -1,9 +1,7 @@
 /* eslint-disable functional/prefer-readonly-type -- state should be writable */
-import {useEffect} from 'react';
+import {useCallback, useEffect} from 'react';
 
 import {routes} from '@/common/consts/routes';
-import {useEvent} from '@/common/hooks/useEvent';
-import {useHasMounted} from '@/common/hooks/useHasMounted';
 import {useRouter} from '@/common/hooks/useRouter';
 import {createEventHub, isClient} from '@/common/utils/utils';
 
@@ -34,13 +32,11 @@ export const authorizationHandler = {
 
 export const useAuthorization = () => {
   const router = useRouter();
-  const hasMounted = useHasMounted();
 
-  const logoutHandler = useEvent(() => {
-    if (hasMounted.current) {
-      void router.push(routes.account.login);
-    }
-  });
+  const logoutHandler = useCallback(
+    () => router.push(routes.account.login),
+    [router],
+  );
 
   useEffect(() => {
     eventHub.on('logout', logoutHandler);
