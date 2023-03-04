0618c52044b6fd0e5dadea017528c66a26c8f177
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-03-04 23:15:50 +0100

    feat: split useOnlineCartState hook

--- src/app/contexts/cartContext/online/useOnlineCartState.ts
+++ src/app/contexts/cartContext/online/useOnlineCartState.ts
@@ -13,24 +13,36 @@ import type {
   CartByTokenQueryVariables,
 } from '@/common/types/generated/graphql';
 
+const useCartByToken = (
+  cartToken: string | null,
+  hasMutationState: boolean,
+) => {
+  const {data: {cart} = {}} = useQuery({
+    queryKey: ['cart-by-token'],
+    queryFn: () =>
+      request<CartByTokenQuery, CartByTokenQueryVariables>(cartByTokenQuery, {
+        cartToken,
+      }),
+    enabled: isClient() && Boolean(cartToken) && !hasMutationState,
+  });
+
+  return cart;
+};
+
 export const useOnlineCartState = (cartToken: string | null) => {
   const {onlineCartMutationState, createOnlineCartMutate} = useCreateOnlineCart(
     {
       cartToken,
     },
   );
-  const {data: {cart: fallbackCart} = {}} = useQuery({
-    queryKey: ['cart-by-token'],
-    queryFn: () =>
-      request<CartByTokenQuery, CartByTokenQueryVariables>(cartByTokenQuery, {
-        cartToken,
-      }),
-    enabled: isClient() && Boolean(cartToken) && !onlineCartMutationState,
-  });
+  const cartByToken = useCartByToken(
+    cartToken,
+    Boolean(onlineCartMutationState),
+  );
 
   const onlineCartState = useMemo(
-    () => onlineCartMutationState ?? getOnlineCartState(fallbackCart),
-    [onlineCartMutationState, fallbackCart],
+    () => onlineCartMutationState ?? getOnlineCartState(cartByToken),
+    [cartByToken, onlineCartMutationState],
   );
 
   return {
